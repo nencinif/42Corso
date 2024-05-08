83aa66ef3fc82e9791c1412dc72a98d785e4fa47
nencinif <62701030+nencinif@users.noreply.github.com>
2024-05-08 17:38:38 +0200

    in teoria completo

--- ft_get_next_line/get_next_line_bonus.c
+++ ft_get_next_line/get_next_line_bonus.c
@@ -6,16 +6,36 @@
 /*   By: fabnenci <fabnenci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 16:56:26 by fabnenci          #+#    #+#             */
-/*   Updated: 2024/04/25 16:36:23 by fabnenci         ###   ########.fr       */
+/*   Updated: 2024/05/08 17:37:14 by fabnenci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
 
-char	*ft_read(int fd, char *selection)
+char	*ft_read(int fd, char *select)
 {
+	ssize_t	bytes;
+	char	*read_sel;
 
+	if (!select)
+		select = ft_calloc(1, sizeof(char));
+	read_sel = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	bytes = 1;
+	while (!ft_strchr(select, '\n') && bytes > 0)
+	{
+		bytes = read(fd, read_sel, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			free (read);
+			free (select);
+			return (NULL);
+		}
+		read_sel[bytes] = '\0';
+		select = ft_strjoin(select, read_sel);
+	}
+	free (read);
+	return (select);
 }
 
 char	*ft_read_line(char *select)
@@ -67,5 +87,15 @@ char	*ft_remaining_file(char *select, char *line)
 
 char	*get_next_line(int fd)
 {
+	char		*line;
+	static char	*page[1024];
 
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd < 1024)
+		return (NULL);
+	page[fd] = ft_read(fd, page[fd]);
+	if (!page[fd])
+		return (NULL);
+	line = ft_read_line(page[fd]);
+	page[fd] = ft_leftover_file(page[fd], line);
+	return (line);
 }
