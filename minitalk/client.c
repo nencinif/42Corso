2f173ea31244d30ae21fac1c6f5e4bbe809a6fd6
nencinif <62701030+nencinif@users.noreply.github.com>
2024-07-11 09:21:37 +0200

    update

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,60 +6,52 @@
 /*   By: fabnenci <fabnenci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 12:51:00 by fabnenci          #+#    #+#             */
-/*   Updated: 2024/07/08 23:06:17 by fabnenci         ###   ########.fr       */
+/*   Updated: 2024/07/09 17:26:44 by fabnenci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void send_char(int pid, char c)
+void	send_char(int pid, char c)
 {
-	int bit;
-	bit = 0;
+	int	bit;
 
+	bit = 0;
 	while (bit < 8)
 	{
 		if (c & (1 << bit))
 			kill(pid, SIGUSR1);
 		else
-			kill(pid,SIGUSR2);
-		usleep(100);
+			kill(pid, SIGUSR2);
+		usleep(150);
 		bit++;
 	}
-
 }
-int	main(int argc, char *argv[])
+
+int	main(int argc,	char *argv[])
 {
-	int	s_pid;
-	char *message;
-	int i;
+	int		s_pid;
+	char	*message;
+	int		i;
 
 	i = 0;
-
 	if (argc != 3)
 	{
-		ft_printf("ERRORE! Gli argomenti devono essere: %s <PID_server> <messaggio>\n", argv[0]);
-        return (1);
-
+		ft_printf("Plz Scrivi: %s <PID_server> <messaggio>\n", argv[0]);
+		return (1);
 	}
-
 	s_pid = ft_atoi(argv[1]);
-	
-	 if (s_pid <= 0)
-    {
-        ft_printf("ERRORE! PID non valido\n");
-        return (1);
-    }
-	
+	if (s_pid <= 0)
+	{
+		ft_printf("ERRORE! PID non valido\n");
+		return (1);
+	}
 	message = argv[2];
-
 	while (message[i] != '\0')
 	{
 		send_char(s_pid, message[i]);
 		i++;
 	}
 	send_char(s_pid, '\0');
-
-	return(0);
-
+	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,42 +6,41 @@
 /*   By: fabnenci <fabnenci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 12:51:07 by fabnenci          #+#    #+#             */
-/*   Updated: 2024/07/08 23:04:26 by fabnenci         ###   ########.fr       */
+/*   Updated: 2024/07/09 16:27:52 by fabnenci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    sighandler(int signum)
+void	sighandler(int signum)
 {
-    static unsigned char c;
-    static int           i;
+	static unsigned char	c;
+	static int				i;
 
-    if (signum == SIGUSR1)
-        c |= 1 << i;
-    i++;
-    if (i == 8)
-    {
-        if (c == 0)
-            write (1,"\n",1);
-        else
-            write(1,&c,1);
-        i = 0;
-        c = 0;
-    }
-   
-
+	if (signum == SIGUSR1)
+		c |= 1 << i;
+	i++;
+	if (i == 8)
+	{
+		if (c == 0)
+			write (1, "\n", 1);
+		else
+			write(1, &c, 1);
+		i = 0;
+		c = 0;
+	}
 }
-int main(void)
-{
-    struct sigaction    sa;
 
-    sa = (struct sigaction){0};
-    sa.sa_handler = sighandler;
-    ft_printf("Server PID: %d\n", getpid());
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-    while (1)
-        pause();
-    return (0);
+int	main(void)
+{
+	struct sigaction	sa;
+
+	sa = (struct sigaction){0};
+	sa.sa_handler = sighandler;
+	ft_printf("Server PID: %d\n", getpid());
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
 }
